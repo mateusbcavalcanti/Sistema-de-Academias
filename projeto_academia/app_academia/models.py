eec56bfd364d4f491450c0beabb0c44fc492414e
Caio Siqueira <caiocesar.siqueira21@gmail.com>
2024-02-17 16:15:04 -0300

    Add Aluno and Exercicio table and improve Treino table

--- projeto_academia/app_academia/models.py
+++ projeto_academia/app_academia/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from datetime import datetime
+
+class Aluno(models.Model):
+    id_aluno = models.AutoField(primary_key=True) # dps a gente muda pra cpf
+    nome_aluno = models.CharField(max_length=255)
+    data_nasc = models.CharField(max_length=10)
+    telefone = models.CharField(max_length=14)
 
 class Treino(models.Model):
     id_treino = models.AutoField(primary_key=True)
-    aluno = models.TextField(max_length=255)
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
     treino = models.TextField(max_length=255)
+    data_criacao = models.CharField(max_length=10, default=datetime.now().strftime('%d/%m/%Y'))
+    exercicios = models.ManyToManyField('Exercicio')
+
+class Exercicio(models.Model):
+    nome_exercicio = models.CharField(max_length=255, primary_key=True)
+    series = models.IntegerField()
+    repeticoes = models.IntegerField()
