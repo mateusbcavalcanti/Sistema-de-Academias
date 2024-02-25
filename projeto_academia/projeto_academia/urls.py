32d32090bdc0c4a6e29acaa7f6cf30dacc6b37c5
Mateus Barbosa de Souza Cavalcanti <98708399+mateusbcavalcanti@users.noreply.github.com>
2024-02-25 11:28:22 -0300

    fixed migrations

--- projeto_academia/projeto_academia/urls.py
+++ projeto_academia/projeto_academia/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Rota, view responsável, nome de referência
-    path('', views.home, name= 'home'),
+    path('', views.home, name='home'),
     path('telaAluno/', views.telaAluno, name='telaAluno'),
     path('telaProf/', views.telaProf, name='telaProf'),
     path('treinos/', views.treinos, name='listagem_treinos'),
